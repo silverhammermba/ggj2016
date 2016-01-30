ca087e2ab6d0defa56cf4e6287634fe2ca25711f
Max <silverhammermba@gmail.com>
2016-01-30 13:45:53 -0500

    refactor, add audio

--- Assets/Scripts/Sentence.cs
+++ Assets/Scripts/Sentence.cs
@@ -4,47 +4,51 @@ using System.Collections;
 
 public class Sentence : MonoBehaviour
 {
-	string before;
-	string correct;
-	string after;
-
-	public Text beforeText;
-	public Text afterText;
-	public RectTransform blank;
-
-	public float blankPad = 10f;
 	public float dropThreshold = 50f;
 
 	public Color correctColor = Color.green;
 	public Color wrongColor = Color.red;
 
-	public void Setup(string bef, string cor, string aft)
+	public string blanks = "_____";
+
+	Text text;
+
+	string sentence;
+	string correct;
+	int blankIndex;
+
+	public void Setup(string sent, string cor)
 	{
-		before = bef;
+		sentence = sent;
 		correct = cor;
-		after = aft;
+
+		if ((blankIndex = sentence.IndexOf("_")) < 0)
+			Debug.Log("Can't find blank in sentence: " + sentence);
+		else
+		{
+			SetBlank(blanks);
+		}
 	}
 
-	void Start()
+	void Awake()
 	{
-		beforeText.text = before;
-		afterText.text = after;
+		text = GetComponent<Text>();
+	}
 
-		blank.localPosition = new Vector3(blankPad + blank.rect.width / 2f, blank.localPosition.y, 0f);
-		afterText.transform.localPosition = new Vector3(2 * blankPad + blank.rect.width, 0f, 0f);
+	void SetBlank(string str)
+	{
+		text.text = sentence.Substring(0, blankIndex) + str + sentence.Substring(blankIndex + 1, sentence.Length - blankIndex - 1);
 	}
 
 	public void FillIn(GameObject word)
 	{
-		float dist = Vector3.Distance(word.transform.position, blank.position);
+		float dist = Vector3.Distance(word.transform.position, transform.position);
 
 		if (dist < dropThreshold)
 		{
-			blank.gameObject.SetActive(false);
 			string eng = word.GetComponent<Noun>().english;
 			string colhex = ColorUtility.ToHtmlStringRGB(eng == correct ? correctColor : wrongColor);
-			afterText.text = " <color=#" + colhex + ">" +  eng + "</color>" + after;
-			afterText.transform.localPosition = new Vector3(0f, 0f, 0f);
+			SetBlank("<color=#" + colhex + ">" +  eng + "</color>");
 		}
 	}
 }
--- Assets/Scripts/TestManager.cs
+++ Assets/Scripts/TestManager.cs
@@ -9,37 +9,65 @@ public class TestManager : MonoBehaviour
 	public RectTransform topBar;
 	public RectTransform bottomBar;
 
-	string[,] tests = new string[3, 4]
+	int sentenceIndex = 0;
+	Sentence currentSentence = null;
+
+	// hardcoded challenges
+	string[,] tests = new string[4, 3]
 	{
-		{"Tom brushes his", "teeth", ".", "牙齿"},
-		{"Tom eats a big", "breakfast", ".", "早餐"},
-		{"Tom takes a", "shower", ".", "洗澡"},
+		{"Tom brushes his _.", "teeth", "牙齿"},
+		{"Tom eats a big _.", "breakfast", "早餐"},
+		{"Tom takes a _.", "shower", "洗澡"},
+		{"Tom needs to wear _.", "clothes", "zh"},
 	};
 
 	void Start()
 	{
-		CreateSentence(0);
+		NextSentence();
 
-		for (int i = 0; i < tests.GetLength(0); ++i)
+		int num = tests.GetLength(0);
+
+		// decide a random order to add nouns
+		int[] indices = new int[num];
+		for (int i = 0; i < num; ++i) indices[i] = i;
+		for (int i = 0; i < num - 1; ++i)
 		{
-			CreateNoun(i);
+			int swp = Random.Range(i, num);
+			int tmp = indices[swp];
+			indices[swp] = indices[i];
+			indices[i] = tmp;
+		}
+
+		// add nouns
+		for (int i = 0; i < num; ++i)
+		{
+			CreateNoun(indices[i]);
 		}
 	}
 
+	public void NextSentence()
+	{
+		if (currentSentence != null)
+		{
+			GameObject.Destroy(currentSentence.gameObject);
+		}
+		CreateSentence(sentenceIndex++);
+	}
+
+	// create a sentence from the hardcoded list
 	void CreateSentence(int i)
 	{
-		// create a sentence
 		GameObject st = Instantiate(sentence, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
-		st.GetComponent<Sentence>().Setup(tests[i, 0], tests[i, 1], tests[i, 2]);
+		currentSentence = st.GetComponent<Sentence>();
+		currentSentence.Setup(tests[i, 0], tests[i, 1]);
 		st.transform.SetParent(topBar);
 		st.transform.localPosition = new Vector3(0f, -50f);
 	}
 
+	// create a noun button from the hardcoded list
 	void CreateNoun(int i)
 	{
-		// and a noun button
-		// TODO add this button to a list that is scrambled up
 		GameObject nn = Instantiate(noun, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
-		nn.GetComponentInChildren<Noun>().Setup(tests[i, 3], tests[i, 1], bottomBar);
+		nn.GetComponentInChildren<Noun>().Setup(tests[i, 2], tests[i, 1], bottomBar);
 	}
 }
